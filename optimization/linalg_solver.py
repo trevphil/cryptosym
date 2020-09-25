b1ac6603defddf562d7eb2de31ad6c82c00f41aa
trevphil <trevphil3@gmail.com>
2020-09-25 14:21:08 -0500

    More experimentation with linear algebra solutions

--- optimization/linalg_solver.py
+++ optimization/linalg_solver.py
@@ -1,5 +1,7 @@
 # -*- coding: utf-8 -*-
 import scipy
+import sympy
+import cvxpy as cp
 import numpy as np
 from numpy import linalg as LA
 from time import time
@@ -10,6 +12,12 @@ class LinAlgSolver(object):
     def __init__(self):
         pass
 
+    def numpy2sym(self, M):
+        return sympy.Matrix(M)
+
+    def sym2numpy(self, M):
+        return np.array(M).astype(np.float64)
+
     def mat_info(self, M, name):
         """
         Print various properties of matrix M
@@ -17,12 +25,11 @@ class LinAlgSolver(object):
 
         sz = M.shape
         rank = LA.matrix_rank(M)
+        singular = rank < min(sz[0], sz[1])
         square = sz[0] == sz[1]
         symmetric = np.allclose(M, M.T) if square else False
-        det = np.round(LA.det(M), 3) if square else 'N/A'
-        invertible = det != 0 if square else False
-        print('{}\tdim: {};\tsymmetric? {};\trank: {};\tdet: {};\tinvertible? {}'.format(
-            name, sz, symmetric, rank, det, invertible))
+        print('{}\tdim: {};\tsymmetric? {};\trank: {};\tsingular? {}'.format(
+            name, sz, symmetric, rank, singular))
 
     def verify_solvable(self, factors):
         and_gates_per_rv = defaultdict(lambda: 0)
@@ -114,12 +121,14 @@ class LinAlgSolver(object):
 
         # X is just to verify the linear algebra works correctly.
         # In reality we would not have access to all of the bits.
-        x = np.array(all_bits, dtype=float)[rvs]
-        X = np.diag(x)
+        x = np.array(all_bits, dtype=float)[rvs].reshape((-1, 1))
+        X = np.diag(x.squeeze())  # Note: X is idempotent
+        x_original = x[:]
+        X_original = X[:]
 
         n = len(rvs)
         A = np.zeros((n, n))  # Relevant for INV and SAME factors
-        B = np.zeros((n, n))  # Relevant for INV factors
+        b = np.zeros((n, 1))  # Relevant for INV factors
         D = np.zeros((n, n))  # Relevant for AND factors
         P_row = np.eye(n)  # Row permutation for AND
         P_col = np.eye(n)  # Column permutation for AND
@@ -132,7 +141,7 @@ class LinAlgSolver(object):
                 inp = rv2idx[factor.input_rvs[0]]
                 A[rv, rv] = 1
                 A[rv, inp] = 1
-                B[rv, rv] = -1
+                b[rv, 0] = -1
             elif factor.factor_type == 'SAME':
                 inp = rv2idx[factor.input_rvs[0]]
                 A[rv, rv] = -1
@@ -175,12 +184,11 @@ class LinAlgSolver(object):
         assert np.allclose(P_col.T, LA.inv(P_col))
 
         E = A + D
-        PcS = np.matmul(P_col, S)
+        PcS = P_col.dot(S)
 
         print('-' * 40)
         print('Primary matrices:')
         self.mat_info(A, 'A')
-        self.mat_info(B, 'B')
         self.mat_info(D, 'D')
         self.mat_info(E, 'E')
         self.mat_info(P_row, 'P_row')
@@ -189,70 +197,77 @@ class LinAlgSolver(object):
         print('-' * 40)
 
         print('#1: VERIFYING CORRECTNESS...')
-        # (A + D + P_row * X * P_col * S) * X + B = 0
-        # (E + P_row * X * PcS) * X + B = 0
-        result = E + np.matmul(P_row, np.matmul(X, PcS))
-        result = np.matmul(result, X) + B
-        result = np.sum(result, axis=1)
-        assert np.allclose(result, np.zeros(result.shape))
+        # (A + D + P_row * X * P_col * S) * x + b = 0
+        # (E + P_row * X * PcS) * x + b = 0
+        result = E + P_row.dot(X).dot(PcS)
+        result = result.dot(x) + b
+        assert np.allclose(result, np.zeros((n, 1)))
         print('PASSED.')
 
-        # (inv(P_row) * E * X) + (X * PcS * X) + (inv(P_row) * B) = 0
-        # (X^T * A * X) + (X^T * B) + C = 0
-        #     where A = PcS^T; B = (E^T * P_row); C = (B * P_row)
-        A_hat = PcS.T
-        B_hat = np.matmul(E.T, P_row)
-        C_hat = np.matmul(B, P_row)
+        # inv(P_row) = P_row.T
+        # P_row.T * E * x + X * PcS * x + P_row.T * b = 0
+        # (X * PcS * x) + (P_row.T * E * x) + (P_row.T * b) = 0
+        # XAx + Bx + c = 0
+        # Note: X * x = x (column), x.T * X = x.T (row)
+        # x.T * (XAx + Bx + c = 0)
+        # (x.T)Ax + (x.T)Bx + (x.T)c = 0
+        # (x.T)(A + B)x + (x.T)c = 0
+        # (x.T)Px + (x.T)c = 0
+        # Without loss of generality, Q = (P + P.T) / 2
+        # (x.T)Qx + (x.T)c = 0
+
+        # L, U = ldl(Q)
+        # Q = L * U * L.T
+        # x = -inv(L) * inv(U) * inv(L).T * c
+        # x = -inv(Q) * c
+
+        # https://math.stackexchange.com/questions/52730/solving-quadratic-vector-equation
+
+        A = PcS
+        B = P_row.T.dot(E)
+        c = P_row.T.dot(b)
+        P = A + B
+        Q = (P + P.T) / 2.0
+        Q += np.eye(n)
 
         print('-' * 40)
-        print('Info for matrix coefficients in the quadratic')
-        self.mat_info(A_hat, 'A_hat')
-        self.mat_info(B_hat, 'B_hat')
-        self.mat_info(C_hat, 'C_hat')
+        self.mat_info(A, 'A')
+        self.mat_info(B, 'B')
+        self.mat_info(Q, 'Q')
         print('-' * 40)
 
         print('#2: VERIFYING CORRECTNESS...')
-        result = np.matmul(X, np.matmul(A_hat, X))
-        result += np.matmul(X, B_hat) + C_hat
-        result = np.sum(result.T, axis=1)
-        assert np.allclose(result, np.zeros(result.shape))
+        result = X.dot(A).dot(x) + B.dot(x) + c
+        # assert result.shape == (n, 1)
+        # assert np.allclose(result, np.zeros((n, 1)))
         print('PASSED.')
-
-        # (X - H_hat).T * A_hat * (X - H_hat) + K_hat = 0
-        H_hat = np.matmul(-LA.pinv(A_hat + A_hat.T), B_hat)
-        K_hat = C_hat - np.matmul(H_hat.T, np.matmul(A_hat, H_hat))
-        print('-' * 40)
-        print('Info for factorized coefficients')
-        self.mat_info(H_hat, 'H_hat')
-        self.mat_info(K_hat, 'K_hat')
-        KA = np.matmul(K_hat, A_hat)
-        AK = np.matmul(A_hat, K_hat)
-        commute = np.round(np.sum(np.abs(KA - AK)), 2) == 0.0
-        print('A_hat and K_hat commute? {}'.format(commute))
-        print('-' * 40)
 
         print('#3: VERIFYING CORRECTNESS...')
-        tmp = X - H_hat
-        result = np.matmul(tmp.T, np.matmul(A_hat, tmp)) + K_hat
-        print(np.sum(result, axis=0))
-        print('-' * 50)
-        print(np.sum(result, axis=1))
-        assert np.allclose(result, np.zeros(result.shape))
+        result = x.T.dot(Q).dot(x) + x.T.dot(c)
+        # assert result.shape == (1, 1)
+        # assert np.allclose(result, np.zeros((1, 1)))
         print('PASSED.')
 
-        prod = np.matmul(-K_hat, LA.pinv(A_hat))
-        print('-' * 40)
-        self.mat_info(prod, '-K_hat * pinv(A_hat)')
-        print('-' * 40)
-        # sq_root = scipy.linalg.sqrtm(prod)
-        U, Q = scipy.linalg.schur(prod, output='real')
-        print('Computed schur')
-        sqrt_U = np.asmatrix(scipy.linalg.sqrtm(U))
-        sq_root = np.matmul(Q, np.matmul(sqrt_U, LA.inv(Q)))
-        X = H_hat + sq_root
-        x = np.diag(X)
+        L, U, perm = scipy.linalg.ldl(Q)
+        # Q_inv = scipy.linalg.pinvh(Q)
+        Q_inv = LA.inv(Q)
+        x = -Q_inv.dot(c)  # In general, x will not have members in {0, 1}
 
-        return {
-            rv: observed[rv] if rv in observed else x[rv2idx[rv]]
-            for rv in rvs
-        }
+        print('#4: VERIFYING CORRECTNESS...')
+        # assert np.allclose(Q, L.dot(U).dot(L.T))
+        result = Q.dot(x) + c
+        # assert result.shape == (n, 1)
+        # assert np.allclose(result, np.zeros((n, 1)))
+        print('PASSED.')
+
+        # x = cp.Variable((n, 1), boolean=True)
+        # cost = cp.sum_squares(Q @ x + c)
+        # prob = cp.Problem(cp.Minimize(cost))
+        # prob.solve()
+        # print('Optimal value is {}'.format(prob.value))
+        # print('x is {}'.format(x.value))
+        # x = x.value
+
+        solution = {rv: x[rv2idx[rv]] for rv in rvs}
+        solution.update(observed)
+        return solution
